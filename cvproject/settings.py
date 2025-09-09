33973c709b9a2e12fafa97517d63339d84168f69
Ulyanafisa16 <ulyanafisa0@gmail.com>
2025-09-09 11:36:49 +0700

    halaman 404

--- cvproject/settings.py
+++ cvproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i=k)x*nwn#gg$s9h$(#sf9jujzpk$k@@n(3@)baayaqfqq@!jr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
